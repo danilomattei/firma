7d420c1416165521090c7582ca6f3d7e32ea7d0f
Danilo Mattei <d22701@oaopava.cz>
2025-05-13 09:05:54 +0200

    username

--- dochazka/models.py
+++ dochazka/models.py
@@ -9,31 +9,32 @@ class Department(models.Model):
         return f"{self.name}"
 
 class CustomUserManager(UserManager):
-    def _create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError("Nebyl zadan email!")
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError("Nebylo zadane uzivatelske jmeno!")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
     
-    def create_user(self, email = None, password = None, **extra_fields):
+    def create_user(self, username = None, password = None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
     
-    def create_superuser(self, email = None, password = None, **extra_fields):
+    def create_superuser(self, username = None, password = None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
 
 class Employee(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=32)
     surname = models.CharField(max_length=32)
-    email = models.EmailField(blank=True, default='', unique=True)
+    username = models.CharField(max_length=64, default='', unique=True)
+    email = models.EmailField(blank=True, default='')
     password = models.CharField(max_length=7)
     department = models.ForeignKey('Department', related_name="employees", on_delete=models.CASCADE, null=True, blank=True)
     position = models.CharField(max_length=32)
@@ -44,7 +45,7 @@ class Employee(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
 
